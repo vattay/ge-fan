f63e63b4272bbae2d055ef839cc92052c46e9c5b
vattay <3martini@gmail.com>
2018-05-26 15:04:38 -0400

    Fixes

--- ge-fan-d.sh
+++ ge-fan-d.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup ./ge-fan 1 >> ~/stasis/ge-fan/log/ge-fan.log 2> ~/stasis/ge-fan/log/ge-fan-err.log &
+nohup ~/stasis/ge-fan/ge-fan 1 >> ~/stasis/ge-fan/log/ge-fan.log 2> ~/stasis/ge-fan/log/ge-fan-err.log &
