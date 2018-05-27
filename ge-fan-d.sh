1a4db4cb0d5f959bfe62318dbc62f4ff50fedd52
vattay <3martini@gmail.com>
2018-05-26 23:35:08 -0400

    Generalize detached logging a bit

--- ge-fan-d.sh
+++ ge-fan-d.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [ -z ${SLOG+x} ]; then 
+    SLOG_STD='/dev/null' 
+    SLOG_ERR='/dev/null'
+    echo "Logging disabled"
+else 
+    SLOG_STD=$SLOG/ge-fan.log 
+    SLOG_ERR=$SLOG/ge-fan-err.log
+    echo "Logging to '$LOGDIR'" 
+fi
 
 mkdir -p log
-nohup ./ge-fan 1> log/ge-fan.log 2> log/ge-fan-err.log &
+nohup ./ge-fan 1> $SLOG_STD 2> $SLOG_ERR &
